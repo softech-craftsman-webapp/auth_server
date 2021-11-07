f439cbbe34b1b1d3651ceb313ca354632537d5d6
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-11-07 20:27:11 +0100

    Updated

--- bootstrap/routes.go
+++ bootstrap/routes.go
@@ -28,25 +28,23 @@ func InitRoutes(app *echo.Echo) {
 	app.GET("/openapi/*", echoSwagger.WrapHandler)
 	app.GET("/openapi", controller.SwaggerRedirect)
 
-	// Auth Controller
-	// RSA Public Key
 	app.GET("/auth/public-key", controller.ServePublicKey)
-
+	app.GET("/auth/verify/:token", auth_controller.VerifyEmail)
 	app.POST("/auth/register", auth_controller.Register)
 	app.POST("/auth/login", auth_controller.Login)
-	app.GET("/auth/verify/:token", auth_controller.VerifyEmail)
 	app.POST("/auth/forgot-password", auth_controller.ForgotPassword)
 	app.POST("/auth/change-password/:token", auth_controller.ChangePassword)
-	access_route.POST("/auth/email-resend", auth_controller.EmailResend)
-	access_route.POST("/auth/logout", auth_controller.Logout)
 
 	// Refresh Zone
 	refresh_route.GET("/auth/refresh", auth_controller.Refresh)
 
+	access_route.POST("/auth/email-resend", auth_controller.EmailResend)
+	access_route.POST("/auth/logout", auth_controller.Logout)
+
 	// User Controller
-	access_route.DELETE("/users/:id", users_controller.DeleteUser)
 	access_route.GET("/users/:id", users_controller.GetUser)
 	access_route.PUT("/users/:id/update-email", users_controller.UpdateEmail)
 	access_route.PUT("/users/:id/update-password", users_controller.UpdatePassword)
 	access_route.PUT("/users/:id/update-name", users_controller.UpdateName)
+	access_route.DELETE("/users/:id", users_controller.DeleteUser)
 }
