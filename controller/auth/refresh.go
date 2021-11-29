34e8ba1f0105a3e4214b34c9e8a01d921ce84d8d
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-11-29 16:18:40 +0100

    Too many connections bugfix

--- controller/auth/refresh.go
+++ controller/auth/refresh.go
@@ -60,14 +60,22 @@ func Refresh(ctx echo.Context) error {
 	   |--------------------------------------------------------------------------
 	*/
 	auth_token := &model.Auth{}
-	db.Where("user_id = ?", user.ID).First(&auth_token)
+	auth_token_get := db.Where("user_id = ?", user.ID).First(&auth_token)
+	if auth_token_get.Error != nil {
+		resp := &view.Response{
+			Success: false,
+			Message: "Auth Token error",
+			Payload: nil,
+		}
+
+		// close db
+		config.CloseDB(db).Close()
+
+		return view.ApiView(http.StatusInternalServerError, ctx, resp)
+	}
 
 	token := auth.GenerateSalt(user.Email, user.ID)
 	auth_token_result := db.Model(&auth_token).Where("user_id = ? AND id = ?", user.ID, auth_token.ID).Update("token", token)
-
-	user.Auth.ID = auth_token.ID
-	user.Auth.Token = token
-
 	if auth_token_result.Error != nil {
 		resp := &view.Response{
 			Success: false,
@@ -81,13 +89,50 @@ func Refresh(ctx echo.Context) error {
 		return view.ApiView(http.StatusInternalServerError, ctx, resp)
 	}
 
+	user.Auth.ID = auth_token.ID
+	user.Auth.Token = token
+
 	token, err_token := auth.CreateAccessToken(
 		view.UserModeltoView(user),
 		controller.ACCESS_TOKEN_VALID_MINUTE)
 
+	/*
+	   |--------------------------------------------------------------------------
+	   | Error Token
+	   |--------------------------------------------------------------------------
+	*/
+	if err_token != nil {
+		resp := &view.Response{
+			Success: true,
+			Message: "Token error",
+			Payload: nil,
+		}
+		// close db
+		config.CloseDB(db).Close()
+
+		return view.ApiView(http.StatusInternalServerError, ctx, resp)
+	}
+
 	refresh_token, err_refresh_token := auth.CreateRefreshToken(
 		view.UserModeltoView(user),
 		controller.REFRESH_TOKEN_VALID_MINUTE)
+
+	/*
+	   |--------------------------------------------------------------------------
+	   | Error Refresh Token
+	   |--------------------------------------------------------------------------
+	*/
+	if err_refresh_token != nil {
+		resp := &view.Response{
+			Success: true,
+			Message: "Refresh Token error",
+			Payload: nil,
+		}
+		// close db
+		config.CloseDB(db).Close()
+
+		return view.ApiView(http.StatusInternalServerError, ctx, resp)
+	}
 
 	resp := &view.Response{
 		Success: true,
@@ -99,22 +144,8 @@ func Refresh(ctx echo.Context) error {
 		},
 	}
 
-	/*
-	   |--------------------------------------------------------------------------
-	   | Main Error
-	   |--------------------------------------------------------------------------
-	*/
-	if err_token != nil || err_refresh_token != nil {
-		resp := &view.Response{
-			Success: true,
-			Message: "Token error",
-			Payload: nil,
-		}
-		// close db
-		config.CloseDB(db).Close()
-
-		return view.ApiView(http.StatusInternalServerError, ctx, resp)
-	}
+	// close db
+	config.CloseDB(db).Close()
 
 	return view.ApiView(http.StatusCreated, ctx, resp)
 }
